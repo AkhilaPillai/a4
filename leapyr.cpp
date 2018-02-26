4f1f4998b6681fe221d0720bfc67136ce46ab483
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-02-26 17:27:07 +0530

    Create leapyr.cpp

---
+++ leapyr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"Enter the n value";
+cin>>n;
+if(n%4==0)
+{
+cout<<"Leap Year";
+}
+else
+{
+cout<<"Not A Leap Year";
+}
+return 0;
+}
